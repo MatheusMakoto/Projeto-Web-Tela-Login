d8e0108b0a547f93994569cdd605ffb617f05e6a
MatheusMakoto <63618818+MatheusMakoto@users.noreply.github.com>
2020-11-19 17:01:53 -0300

    Atualização 1

---
+++ Projeto_Web/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const { ErrorHandlerMiddleware } = require("./middlewares");
+const { userRouter } = require("./routers");
+
+mongoose.connect(
+  "mongodb+srv://unifeso:unifeso-password@unifeso.kwuxv.gcp.mongodb.net/unifeso-financial-control?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error: "));
+db.once("open", function () {
+  console.log("MongoDB Connected.");
+});
+
+const app = express();
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use("/users", userRouter);
+app.use(ErrorHandlerMiddleware);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+}); 
